0f8fe28d1572779d95bd3a6fc689e39330a82e72
Julio Alberto de la Teja López <jdelatejal242@profesor.uemex.mx>
2022-08-13 21:34:23 -0500

    arreglos

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,10 @@
 public class Principal {
     public static void main(String[] args) {
         System.out.println("hola");
+        //variable
+        int var1=10;
+        System.out.println(var1);
+        var1++;
+        System.out.println(var1);
     }
 }
