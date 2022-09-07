62435bf25259ce73faaa9c728e1e9c360b94d111
Julio Alberto de la Teja López <jdelatejal242@profesor.uemex.mx>
2022-09-07 09:59:55 -0500

    Herencia y clase abstracta Clase 0907

--- src/figuras/prueba.java
+++ src/figuras/prueba.java
@@ -2,7 +2,7 @@ package figuras;
 
 public class prueba {
     public static void main(String[] args) {
-        Triangulo t1 = new Triangulo();
-        //Figura f1 =new Figura();
+        Triangulo t1 = new Triangulo(2,3,4);
+        System.out.println(t1.toString());
     }
 }
