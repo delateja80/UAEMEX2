9eda6368830281eab2a614aa9edd44257949a6e3
Julio Alberto de la Teja López <jdelatejal242@profesor.uemex.mx>
2022-09-07 11:26:57 -0500

    Herencia y clase abstracta Clase 0907 ***extra de polimorfismo II

--- src/figuras/prueba.java
+++ src/figuras/prueba.java
@@ -2,6 +2,13 @@ package figuras;
 
 public class prueba {
     public static void main(String[] args) {
+        Figura[] figs=new Figura[3];
+        figs[0]=new Cuadrado(2);
+        figs[1]=new Triangulo(4,4,4);
+        figs[2]=new Cuadrado(4);
 
+        for(Figura fg:figs){
+            System.out.println(fg.toString());
+        }
     }
 }
